74b713b717546e3dfc786f2eaeb059fea66d4376
seiche-publish <noreply@seiche>
2026-08-09 09:35:47 +0000

    snapshot 2026-08-09T09:35Z

--- assets/Basin-z_1OA2NH.js
+++ assets/Basin-z_1OA2NH.js
@@ -1,4 +1,4 @@
-import{r as c,j as U}from"./index-DYRQ1S7X.js";const C=`
+import{r as c,j as U}from"./index-C2St8ZlW.js";const C=`
 attribute vec2 p;
 void main() { gl_Position = vec4(p, 0.0, 1.0); }
 `,P=`
