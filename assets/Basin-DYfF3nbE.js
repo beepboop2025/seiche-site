a6d4aba709e59be282af1558698a9705bf2ddc77
seiche-publish <noreply@seiche>
2026-08-12 17:02:30 +0000

    snapshot 2026-08-12T17:02Z

--- assets/Basin-DYfF3nbE.js
+++ assets/Basin-DYfF3nbE.js
@@ -1,4 +1,4 @@
-import{r as c,j as U}from"./index-DglItKRx.js";const C=`
+import{r as c,j as U}from"./index-D76-0BO5.js";const C=`
 attribute vec2 p;
 void main() { gl_Position = vec4(p, 0.0, 1.0); }
 `,P=`
