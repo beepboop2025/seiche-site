0e0f6e39dcdb872a3eaaa184f45824baebf3e52b
seiche-publish <noreply@seiche>
2026-08-22 13:24:58 +0000

    snapshot 2026-08-22T13:24Z

--- assets/Basin-BMtTDo3J.js
+++ assets/Basin-BMtTDo3J.js
@@ -1,4 +1,4 @@
-import{r as c,j as U}from"./index-D7I9LFbP.js";const C=`
+import{r as c,j as U}from"./index-DKgfzM6i.js";const C=`
 attribute vec2 p;
 void main() { gl_Position = vec4(p, 0.0, 1.0); }
 `,P=`
