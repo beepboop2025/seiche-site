98a199aaebe397c44960759f1fd749395e63c41c
seiche-publish <noreply@seiche>
2026-08-22 02:34:15 +0000

    snapshot 2026-08-22T02:34Z

--- assets/Basin-CdeIUVRD.js
+++ assets/Basin-CdeIUVRD.js
@@ -1,4 +1,4 @@
-import{r as c,j as U}from"./index-DVfip0DK.js";const C=`
+import{r as c,j as U}from"./index--sE2bwQD.js";const C=`
 attribute vec2 p;
 void main() { gl_Position = vec4(p, 0.0, 1.0); }
 `,P=`
